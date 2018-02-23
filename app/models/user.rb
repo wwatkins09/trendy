093dc3d2ba2b0c88d67a5c1b8bdc4c22ca76fdb9
Will <willwatkins9@gmail.com>
2018-02-23 14:56:41 -0500

    Add session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
 end
