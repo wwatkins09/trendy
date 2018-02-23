2841ba0726ebea9bbc10553841b998196d5bc9b1
Will <willwatkins9@gmail.com>
2018-02-23 17:10:00 -0500

    Add App component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import App from './app';
 
-const Root = (props) => {
+const Root = ({store}) => {
   return (
-    <div>ROOT!!!!!</div>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   );
 }
 
