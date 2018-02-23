78d99a9cb9ab839a1cd6fcfe2786826cef7a39f8
Will <willwatkins9@gmail.com>
2018-02-23 15:08:34 -0500

    Add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
+  attr_reader :password
+
   def ensure_session_token
     self.session_token ||= SecureRandom.urlsafe_base64
   end
@@ -13,6 +15,15 @@ class User < ApplicationRecord
     self.session_token = SecureRandom.urlsafe_base64
     self.save!
     self.session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
 end
