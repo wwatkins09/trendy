1ec3fdd0f5bb36797e348da192e80496dc28b077
Will <willwatkins9@gmail.com>
2018-02-24 17:36:35 -0500

    Bootstrap current user

--- frontend/trendy.jsx
+++ frontend/trendy.jsx
@@ -4,7 +4,14 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { users: {[window.currentUser.user.id]: window.currentUser}, session: {currentUserId: window.currentUser.id}}
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
