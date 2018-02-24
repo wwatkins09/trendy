b6a70b9b8748707f9807a3b32bd9a6b211c1af65
Will <willwatkins9@gmail.com>
2018-02-24 12:06:08 -0500

    Add signin action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,11 +1,28 @@
 import * as APISessionUtil from '../util/api_session_util';
 
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
-
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const signIn = (credentials) => {
   return (dispatch) => {
-    return APISessionUtil.createSession(credentials);
+    return APISessionUtil.createSession(credentials).then((user) => {
+      dispatch(receiveCurrentUser(user));
+    }, (errors) => {
+      dispatch(receiveSessionErrors(errors.responseJSON));
+    });
   }
 }
+
+export const receiveCurrentUser = (user) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    user
+  };
+};
+
+export const receiveSessionErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
