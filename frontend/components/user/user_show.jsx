01cb0a4d0616beb3a445fdcce6fb8705a9665579
Will <willwatkins9@gmail.com>
2018-02-24 18:24:23 -0500

    Add signout

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -4,11 +4,21 @@ class UserShow extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
+
+  handleSignOut(event) {
+    event.preventDefault();
+    this.props.signOut();
   }
 
   render() {
     return (
-      <div>LOGGED IN!</div>
+      <div>
+        <h1>This is your profile page!</h1>
+        <button onClick={this.handleSignOut}>Sign out</button>
+      </div>
     )
   }
 }
--- frontend/components/user/user_show_container.js
+++ frontend/components/user/user_show_container.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import UserShow from './user_show';
+import {signOut} from '../../actions/session_actions';
 
 const mapStateToProps = (state) => {
   return {
@@ -10,7 +11,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    signOut: () => dispatch(signOut())
   };
 };
 
