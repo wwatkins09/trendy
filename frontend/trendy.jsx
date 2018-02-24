e8e77eff19a003167ff62b46fd4b29b166a98e1a
Will <willwatkins9@gmail.com>
2018-02-24 17:39:06 -0500

    Fix typo

--- frontend/trendy.jsx
+++ frontend/trendy.jsx
@@ -6,12 +6,13 @@ import configureStore from './store/store';
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
-    const preloadedState = { users: {[window.currentUser.user.id]: window.currentUser}, session: {currentUserId: window.currentUser.id}}
+    const preloadedState = { users: {[window.currentUser.id]: window.currentUser}, session: {currentUserId: window.currentUser.id}}
     store = configureStore(preloadedState);
     delete window.currentUser;
   } else {
     store = configureStore();
   }
+  window.store = store;
   const rootEl = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, rootEl);
 });
