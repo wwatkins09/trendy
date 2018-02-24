2e9ca379be7005ec3d34a2f1313938617846f5b4
Will <willwatkins9@gmail.com>
2018-02-24 17:17:01 -0500

    Add new user actions

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,5 +1,22 @@
+import * as APIUserUtil from '../util/api_user_util';
+import {receiveCurrentUser} from './session_actions';
+
 export const RECEIVE_USER = 'RECEIVE_USER';
+export const RECEIVE_USER_ERRORS = 'RECEIVE_USER_ERRORS';
 
 export const signUp = (credentials) => {
-  
+  return (dispatch) => {
+    return APIUserUtil.createUser(credentials).then((user) => {
+      dispatch(receiveCurrentUser(user));
+    }, (errors) => {
+      dispatch(receiveUserErrors(errors.responseJSON));
+    });
+  }
+}
+
+export const receiveUserErrors = (errors) => {
+  return {
+    type: RECEIVE_USER_ERRORS,
+    errors
+  }
 }
