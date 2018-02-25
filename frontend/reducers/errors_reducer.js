24d2e760ddbf054de47998c15a9ef005ab4d4952
Will <willwatkins9@gmail.com>
2018-02-25 13:51:25 -0500

    Add errors to state

---
+++ frontend/reducers/errors_reducer.js
@@ -0,0 +1,8 @@
+const errorsReducer = (oldState = [], action) => {
+  switch(action.type) {
+    default:
+      return oldState;
+  }
+}
+
+export default errorsReducer;
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import sessionReducer from './session_reducer';
+import errorsReducer from './errors_reducer';
 
 export default combineReducers({
   users: usersReducer,
-  session: sessionReducer
+  session: sessionReducer,
+  errors: errorsReducer
 });
