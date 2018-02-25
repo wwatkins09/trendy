3593c64be9028094c7523d0e142e0eb6d3022ea8
Will <willwatkins9@gmail.com>
2018-02-25 14:56:36 -0500

    Add event actions

---
+++ frontend/actions/event_actions.js
@@ -0,0 +1,18 @@
+import * as APIEventUtil from '../util/api_event_util';
+
+export const RECEIVE_EVENTS = 'RECEIVE_EVENTS';
+
+export const fetchEventsByUserId = (userId) => {
+  return (dispatch) => {
+    return APIEventUtil.fetchEventsByUserId(userId).then((events) => {
+      return receiveEvents(events);
+    })
+  }
+}
+
+export const receiveEvents = (events) => {
+  return {
+    type: RECEIVE_EVENTS,
+    events
+  }
+}
---
+++ frontend/util/api_event_util.js
@@ -0,0 +1,7 @@
+export const fetchEventsByUserId = (userId) => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/events'
+    data: {user_id: userId}
+  })
+}
