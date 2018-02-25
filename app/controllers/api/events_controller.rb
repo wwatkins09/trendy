486dd288d52b4cfb06fb29a4d149da569d8e62af
Will <willwatkins9@gmail.com>
2018-02-25 15:16:13 -0500

    Add show view

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -9,12 +9,24 @@ class Api::EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    if (@event.save)
+      render 'api/events/show'
+    else
+      render json: @event.errors.full_messages, status: 422
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:type, :user_id, :quantity, :quality, :duration)
   end
 
 end
