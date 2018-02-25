3ed94bcd799c5941a0485424a3fc46a1892e8ae0
Will <willwatkins9@gmail.com>
2018-02-25 13:53:15 -0500

    Render errors

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,5 +1,12 @@
+import {RECEIVE_SESSION_ERRORS} from '../actions/session_actions';
+import {RECEIVE_USER_ERRORS} from '../actions/user_actions';
+
 const errorsReducer = (oldState = [], action) => {
   switch(action.type) {
+    case RECEIVE_SESSION_ERRORS:
+      return action.errors;
+    case RECEIVE_USER_ERRORS:
+      return action.errors;
     default:
       return oldState;
   }
