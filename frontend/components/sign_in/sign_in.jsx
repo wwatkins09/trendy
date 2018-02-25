7f042b3ccf9355c42aff6358f4e64b837ceda515
Will <willwatkins9@gmail.com>
2018-02-25 13:27:12 -0500

    Begin styling

--- frontend/components/sign_in/sign_in.jsx
+++ frontend/components/sign_in/sign_in.jsx
@@ -29,12 +29,15 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Sign In</h1>
+      <div id="sign-in">
+        <h1>Welcome to Trendy!</h1>
+        <h2>Sign In</h2>
         <input type="email" placeholder="Email" value={this.state.email} onChange={this.handleInput('email')}/>
         <input type="password" placeholder="Password" value={this.state.password} onChange={this.handleInput('password')}/>
-        <button onClick={this.handleSignIn}>Sign in</button>
-        <button onClick={this.handleSignUp}>Sign up</button>
+        <span id="sign-in-buttons">
+          <button onClick={this.handleSignIn}>Sign in</button>
+          <button onClick={this.handleSignUp}>Sign up</button>
+        </span>
       </div>
     );
   }
