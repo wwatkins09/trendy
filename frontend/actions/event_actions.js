36979ff8be97c50433d2747b5f83f8f0e665c0f5
Will <willwatkins9@gmail.com>
2018-02-28 13:29:49 -0500

    Add creation action

--- frontend/actions/event_actions.js
+++ frontend/actions/event_actions.js
@@ -1,6 +1,7 @@
 import * as APIEventUtil from '../util/api_event_util';
 
 export const RECEIVE_EVENTS = 'RECEIVE_EVENTS';
+export const RECEIVE_EVENT = 'RECEIVE_EVENT';
 
 export const fetchEventsByUserId = (userId) => {
   return (dispatch) => {
@@ -10,9 +11,24 @@ export const fetchEventsByUserId = (userId) => {
   }
 }
 
+export const createEvent = (event) => {
+  return (dispatch) => {
+    return APIEventUtil.createEvent(event).then((event) => {
+      dispatch(receiveEvent(event))
+    })
+  }
+}
+
 export const receiveEvents = (events) => {
   return {
     type: RECEIVE_EVENTS,
     events
+  }
+}
+
+export const receiveEvent = (event) => {
+  return {
+    type: RECEIVE_EVENT,
+    event
   }
 }
--- frontend/util/api_event_util.js
+++ frontend/util/api_event_util.js
@@ -3,5 +3,13 @@ export const fetchEventsByUserId = (userId) => {
     method: 'GET',
     url: 'api/events',
     data: {user_id: userId}
-  })
-}
+  });
+};
+
+export const createEvent = (event) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/events',
+    data: {event}
+  });
+};
