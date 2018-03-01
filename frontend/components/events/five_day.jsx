0c0ef003c8017f14ba3139e2b30d1b7e10f3186c
Will <willwatkins9@gmail.com>
2018-02-28 21:48:56 -0500

    create fiveday component

---
+++ frontend/components/events/five_day.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class FiveDay extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>FIVE DAY</div>
+    )
+  }
+}
+
+export default FiveDay;
---
+++ frontend/components/events/five_day_container.js
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import FiveDay from './five_days';
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FiveDay));
