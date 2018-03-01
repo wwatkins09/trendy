793da9a564a6fbd72fe54abdc6786c84769ef2ac
Will <willwatkins9@gmail.com>
2018-02-28 23:14:39 -0500

    Render checks

--- frontend/components/events/five_day_index_item.jsx
+++ frontend/components/events/five_day_index_item.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+const findEvent = (events, offset, today) => {
+  return events.find((event) => event.date === today.getTime() / 1000 - (86400 * offset));
+}
+
 const FiveDayIndexItem = ({category, events}) => {
 
-  
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const days = Array(5).fill(null).map((el, idx) => {
+    const content = findEvent(events, idx, today) ? '✓' : 'X';
+    return (<td key={idx}>{content}</td>);
+  })
 
   return (
     <tr>
-
+      <td>{category}</td>
+      {days}
     </tr>
   );
 };
