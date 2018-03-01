3db73259548d28183a5691e4e7ec4e32de415236
Will <willwatkins9@gmail.com>
2018-03-01 14:10:35 -0500

    Add circles

--- frontend/components/events/five_day_index_item.jsx
+++ frontend/components/events/five_day_index_item.jsx
@@ -4,22 +4,44 @@ const findEvent = (events, offset, today) => {
   return events.find((event) => event.date === today.getTime() / 1000 - (86400 * offset));
 }
 
-const FiveDayIndexItem = ({category, events}) => {
+class FiveDayIndexItem extends React.Component {
 
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  const days = Array(5).fill(null).map((el, idx) => {
-    const content = findEvent(events, idx, today) ? '✓' : 'X';
-    return (<td key={idx}>{content}</td>);
-  })
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <tr>
-      <td>{events.length / 5 * 100}%</td>
-      <td>{category}</td>
-      {days}
-    </tr>
-  );
-};
+  componentDidMount() {
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const canvas = document.getElementById(`circle-${this.props.category}`);
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.arc(20, 20, 10, 0, (Math.PI * 2) * (this.props.events.length / 5), true)
+    ctx.stroke();
+  }
+
+  render() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const days = Array(5).fill(null).map((el, idx) => {
+      const content = findEvent(this.props.events, idx, today) ? '✓' : 'X';
+      return (<td key={idx}>{content}</td>);
+    })
+
+    return (
+      <tr>
+        <td><canvas id={`circle-${this.props.category}`} /></td>
+        <td>{this.props.category}</td>
+        {days}
+      </tr>
+    );
+  }
+}
 
 export default FiveDayIndexItem;
