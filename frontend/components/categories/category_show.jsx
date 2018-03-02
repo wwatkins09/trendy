99e7e3f013baa327e63b89dd658fea81df3efeff
Will <willwatkins9@gmail.com>
2018-03-02 11:23:00 -0500

    Create new components

---
+++ frontend/components/categories/category_show.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class CategoryShow extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>CATEGORY SHOW</div>
+    );
+  }
+}
+
+export default CategoryShow;
