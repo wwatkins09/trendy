6be8cd8e7d9db851f7c2c6080025fc69361fd639
Will <willwatkins9@gmail.com>
2018-03-01 23:26:10 -0500

    Import fetching action

--- frontend/components/categories/category_index_container.js
+++ frontend/components/categories/category_index_container.js
@@ -1,16 +1,17 @@
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import CategoryIndex from './category_index';
+import {fetchEventsByCategoryId} from '../../actions/event_actions';
 
 const mapStateToProps = (state) => {
   return {
-
+    categories: Object.values(state.categories)
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    fetchEventsByCategoryId: (categoryId) => dispatch(fetchEventsByCategoryId(categoryId))
   };
 };
 
