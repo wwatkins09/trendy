47c9f6b77dacad395bdc72add4379093c0f4f5a2
Will <willwatkins9@gmail.com>
2018-03-02 11:11:22 -0500

    Add new categories to frontend

--- frontend/reducers/categories_reducer.js
+++ frontend/reducers/categories_reducer.js
@@ -1,9 +1,11 @@
-import {RECEIVE_CATEGORIES} from '../actions/category_actions';
+import {RECEIVE_CATEGORIES, RECEIVE_CATEGORY} from '../actions/category_actions';
 
 const categoriesReducer = (oldState = {}, action) => {
   switch (action.type) {
     case RECEIVE_CATEGORIES:
-      return Object.assign({}, oldState, action.categories)
+      return Object.assign({}, oldState, action.categories);
+    case RECEIVE_CATEGORY:
+      return Object.assign({}, oldState, {[action.payload.category.id]: action.payload.category})
     default:
       return oldState;
   }
--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -1,11 +1,11 @@
 import {RECEIVE_CURRENT_USER} from '../actions/session_actions';
-import {RECEIVE_EVENT} from '../actions/event_actions';
+import {RECEIVE_CATEGORY} from '../actions/category_actions';
 
 const usersReducer = (oldState = {}, action) => {
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
       return Object.assign({}, oldState, {[action.user.id]: action.user});
-    case RECEIVE_EVENT:
+    case RECEIVE_CATEGORY:
       return Object.assign({}, oldState, {[action.payload.user.id]: action.payload.user})
     default:
       return oldState;
