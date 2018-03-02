68e6cffce93518f7e9b37711df419780e842d5bc
Will <willwatkins9@gmail.com>
2018-03-01 21:33:41 -0500

    Fetch categories

--- frontend/reducers/categories_reducer.js
+++ frontend/reducers/categories_reducer.js
@@ -1,5 +1,9 @@
+import {RECEIVE_CATEGORIES} from '../actions/category_actions';
+
 const categoriesReducer = (oldState = {}, action) => {
   switch (action.type) {
+    case RECEIVE_CATEGORIES:
+      return Object.assign({}, oldState, action.categories)
     default:
       return oldState;
   }
