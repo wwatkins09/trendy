3ca04205b153974da087e167955aefb89d1ff9b4
Will <willwatkins9@gmail.com>
2018-03-01 23:37:22 -0500

    Create table/rows

--- frontend/components/categories/category_index.jsx
+++ frontend/components/categories/category_index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import CategoryIndexItem from './category_index_item';
+
+const dayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 class CategoryIndex extends React.Component {
 
@@ -6,9 +9,40 @@ class CategoryIndex extends React.Component {
     super(props);
   }
 
+  createDate(offset) {
+    return new Date(new Date().setDate(new Date().getDate()-offset));
+  }
+
   render() {
+    const headers = new Array(5).fill(null).map((el, idx) => {
+      const day = this.createDate(idx);
+      return (
+        <th key={idx}>
+          <span>
+            {dayArr[day.getDay()]}
+            {day.getDate()}
+          </span>
+        </th>
+      );
+    });
+
+    const rows = this.props.categories.map((category, idx) => {
+      return <CategoryIndexItem key={idx} category={category} fetchEventsByCategoryId={this.props.fetchEventsByCategoryId} />
+    })
+
     return (
-      <div>CATEGORY SHOW</div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Category</th>
+            {headers}
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
     );
   }
 }
---
+++ frontend/components/categories/category_index_item.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class CategoryIndexItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <tr>
+      </tr>
+    );
+  }
+}
+
+export default CategoryIndexItem;
