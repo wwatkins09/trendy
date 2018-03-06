c70f2f2d95d5ac8f32a63ce7eb29a45bd2f04241
Will <willwatkins9@gmail.com>
2018-03-05 22:10:40 -0500

    Add delete button

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -4,15 +4,31 @@ class EventIndexItem extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(event) {
+    event.preventDefault();
+    this.props.deleteEvent(this.props.event.id);
   }
 
   render() {
     let content;
+    let button;
     if (this.props.event) {
-      content = new Date(this.props.event.date * 1000).toDateString();
+      content = (
+        <span>{new Date(this.props.event.date * 1000).toDateString()}</span>
+      );
+      button = (
+        <button onClick={this.handleDelete}>Delete</button>
+      )
     }
     return (
-      <div>{content}</div>
+      <div>
+        {content}
+        {button}
+      </div>
     );
   }
 }
