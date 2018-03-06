97e6a9cca8310ff31fed4091ea0a78ca403c82b8
Will <willwatkins9@gmail.com>
2018-03-05 22:46:27 -0500

    Update category slice of state

--- frontend/reducers/categories_reducer.js
+++ frontend/reducers/categories_reducer.js
@@ -1,11 +1,21 @@
+import merge from 'lodash/merge';
 import {RECEIVE_CATEGORIES, RECEIVE_CATEGORY} from '../actions/category_actions';
+import {RECEIVE_EVENT, REMOVE_EVENT} from '../actions/event_actions';
 
 const categoriesReducer = (oldState = {}, action) => {
+  let newState;
   switch (action.type) {
     case RECEIVE_CATEGORIES:
       return Object.assign({}, oldState, action.categories);
     case RECEIVE_CATEGORY:
-      return Object.assign({}, oldState, {[action.payload.category.id]: action.payload.category})
+      return Object.assign({}, oldState, {[action.payload.category.id]: action.payload.category});
+    case RECEIVE_EVENT:
+      newState = Object.assign({}, oldState, {[action.payload.category.id]: action.payload.category});
+      return newState;
+    case REMOVE_EVENT:
+      newState = merge({}, oldState);
+      newState[action.payload.category.id].eventIds = newState[action.payload.category.id].eventIds.filter((eventId) => eventId !== action.payload.event.id);
+      return newState;
     default:
       return oldState;
   }
