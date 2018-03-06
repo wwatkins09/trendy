d2ee0628fab10b6792ff5b2173c4ad5d81ee3b09
Will <willwatkins9@gmail.com>
2018-03-05 20:08:39 -0500

    Sort dates

--- frontend/components/events/event_index.jsx
+++ frontend/components/events/event_index.jsx
@@ -5,6 +5,7 @@ const dayArr = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const today = new Date();
 today.setHours(0, 0, 0, 0);
 
+
 class EventIndex extends React.Component {
 
   constructor(props) {
@@ -15,6 +16,10 @@ class EventIndex extends React.Component {
 
   componentDidMount() {
     this.props.fetchEventsByCategoryId(this.props.categoryId)
+  }
+
+  compareDates(event1, event2) {
+    return event1.date - event2.date
   }
 
   createDate(offset) {
@@ -37,7 +42,7 @@ class EventIndex extends React.Component {
     //     </th>
     //   );
     // });
-    const events = this.props.events.map((event, idx) => {
+    const events = this.props.events.sort(this.compareDates).map((event, idx) => {
       return (
         <EventIndexItem key={idx} event={event} />
       );
