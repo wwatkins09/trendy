9ef9218af518cd2b7f516a15fcd3ccb3f400c0fe
Will <willwatkins9@gmail.com>
2018-03-07 12:22:59 -0500

    Make quantity 1-10

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   validates :category_id, :date, presence: true
   validates :quantity, :quality, :duration, numericality: { greater_than_or_equal_to: 0 }
   validates :date, numericality: { less_than_or_equal_to: Date.today.to_time.to_i }
+  validates :quality, numericality: { less_than_or_equal_to: 10, greater_than_or_equal_to: 1}
   validates_uniqueness_of :date, scope: [:category_id]
 
   belongs_to :category
