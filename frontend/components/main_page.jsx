10310669a001aa6a667b49919312d8845b113e69
Will <willwatkins9@gmail.com>
2018-03-07 12:43:41 -0500

    Add frontend route

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -1,10 +1,11 @@
-import {Route, withRouter} from 'react-router-dom';
+import {Route, withRouter, Switch} from 'react-router-dom';
 import {AuthRoute, ProtectedRoute} from '../util/route_util';
 import React from 'react';
 import {connect} from 'react-redux';
 import SignInContainer from './sign_in/sign_in_container';
 import UserShowContainer from './user/user_show_container';
 import CategoryShowContainer from './categories/category_show_container';
+import LineGraphContainer from './line_graph/line_graph_container';
 
 
 const mapStateToProps = (state) => {
@@ -17,7 +18,10 @@ const MainPage = (props) => {
   return (
     <div>
       <ProtectedRoute path="/users/:userId" component={UserShowContainer} />
-      <ProtectedRoute path="/categories/:categoryId" component={CategoryShowContainer} />
+      <Switch>
+        <ProtectedRoute path="/categories/:categoryId/line-graph" component={LineGraphContainer} />
+        <ProtectedRoute path="/categories/:categoryId" component={CategoryShowContainer} />
+      </Switch>
       <AuthRoute path="/" component={SignInContainer} />
     </div>
   );
