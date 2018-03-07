b0c14b7c58e19935648bec678d769fb2d6420708
Will <willwatkins9@gmail.com>
2018-03-07 12:40:06 -0500

    Create components

---
+++ frontend/components/line_graph/line_graph.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LineGraph extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>LINE GRAPH</div>
+    )
+  }
+}
+
+export default LineGraph;
---
+++ frontend/components/line_graph/line_graph_container.js
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import LineGraph from './line_graph';
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LineGraph));
