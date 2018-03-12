09449a105292099dff23a8b42443c84b50a14a4e
Will <willwatkins9@gmail.com>
2018-03-12 15:54:37 -0400

    Fetch category and events

--- frontend/components/bar_graph/bar_graph.jsx
+++ frontend/components/bar_graph/bar_graph.jsx
@@ -7,9 +7,30 @@ class BarGraph extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    if (!this.props.category.id) {
+      this.props.fetchCategoryById(this.props.categoryId);
+    }
+    this.props.fetchThreeMonthsOfEventsByCategoryId(this.props.categoryId);
+    this.updateCanvas();
+  }
+
+  componentDidUpdate() {
+    this.updateCanvas();
+  }
+
+  updateCanvas() {
+    const canvas = this.refs.bargraph;
+    const ctx = canvas.getContext('2d');
+    canvas.height = 550;
+    canvas.width = 550;
+  }
+
   render() {
     return (
-      <div>BAR GRAPH</div>
+      <div>
+        <canvas ref="bargraph" id="bar-graph" />
+      </div>
     );
   }
 }
--- frontend/components/bar_graph/bar_graph_container.js
+++ frontend/components/bar_graph/bar_graph_container.js
@@ -1,16 +1,24 @@
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 import BarGraph from './bar_graph';
+import {fetchCategoryById} from '../../actions/category_actions';
+import {fetchThreeMonthsOfEventsByCategoryId} from '../../actions/event_actions';
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+  const categoryId = parseInt(ownProps.match.params.categoryId);
+  const category = state.categories[categoryId] || {name: ''}
+  const events = Object.values(state.events).filter((event) => (event.categoryId === categoryId && event.date >= (new Date().getTime() / 1000 - 7776000)))
   return {
-
+    categoryId,
+    category,
+    events
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    fetchCategoryById: (categoryId) => dispatch(fetchCategoryById(categoryId)),
+    fetchThreeMonthsOfEventsByCategoryId: (categoryId) => dispatch(fetchThreeMonthsOfEventsByCategoryId(categoryId))
   };
 };
 
