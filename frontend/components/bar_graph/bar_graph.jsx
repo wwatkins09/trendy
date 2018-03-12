fab55c907f41d885dd66bb160362276c8283e77b
Will <willwatkins9@gmail.com>
2018-03-12 15:41:18 -0400

    Add components

---
+++ frontend/components/bar_graph/bar_graph.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class BarGraph extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>BAR GRAPH</div>
+    );
+  }
+}
---
+++ frontend/components/bar_graph/bar_graph_container.js
@@ -0,0 +1,17 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import BarGraph from './bar_graph';
+
+const mapStateToProps = (state) => {
+  return {
+
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(BarGraph));
